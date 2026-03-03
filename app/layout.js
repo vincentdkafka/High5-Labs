0649a70e59d2a7d3618c2b56bca2856e5495d5fe
Harsh Kurware <harshkurware03@gmail.com>
2026-03-03 19:08:26 +0530

    resloved styling issue

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={poppins.className}
+        className={poppins.className} 
       >
         {children}
       </body>
